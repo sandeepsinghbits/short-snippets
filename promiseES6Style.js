66659cc566a19c597aa231e6e2a07d6942f90264
Sandeep Singh Yadav <33983417+sandeepsinghbits@users.noreply.github.com>
2017-11-28 20:31:52 +0530

    Create promiseES6Style.js

---
+++ promiseES6Style.js
@@ -0,0 +1,40 @@
+function promise(fn){
+   var success=[],failure=[];
+   this.then = function(successFn,errorFn){
+      success.push(successFn);
+      failure.push(errorFn);
+      return this;
+   };
+   function resolve(data){
+      var val = data;
+      success.forEach(function(fun){
+         val = fun(val);
+      });
+   }
+
+   function reject(data){
+      var val = data;
+      failure.forEach(function(fun){
+         val = fun(val);
+      });
+   }
+   fn(resolve,reject);
+}
+
+var p1 = new promise((resolve,reject)=>{
+   setTimeout(()=>{
+      resolve({'name':'sandeep'});
+   },2000);
+})
+
+//Sample Usage
+p1.then(function success(data){
+   console.log("success",data);
+   return data
+}).then(function(data){
+   data.age = 26;
+   return data;
+}).then(function(data){
+   data.company = 'paytm';
+   console.log(data);
+})
