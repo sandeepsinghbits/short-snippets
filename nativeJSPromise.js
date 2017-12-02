bcdbba22fba3b3e5c0778e606f80b84e4845231d
Sandeep Singh Yadav <33983417+sandeepsinghbits@users.noreply.github.com>
2017-12-03 00:06:47 +0530

    Update nativeJSPromise.js

--- nativeJSPromise.js
+++ nativeJSPromise.js
@@ -1,20 +1,45 @@
-
 var $q = (function (){
 
   function promise(){
-    var success=[],failure=[];
-    this.promise = {
-      then:function(successFn,failureFn){
+    var success=[];
+    var failure=[];
+    this.resolvedValue =null;
+    var interval = null;
+    this.then = function(successFn,failureFn){
         success.push(successFn);
         failure.push(failureFn);
         return this;
       }
-    };
     this.resolve = function(obj){
-      var val = obj;
-      success.forEach(function(fun){
-        val = fun(val);
-      });
+      this.resolvedValue = obj;
+      var val = obj,i=0;
+      function iterate(iteration){
+        val = iteration(val);
+        if(val && val.then){
+          interval = setInterval(function(){
+             if(val.resolvedValue !==null){
+              val = val.resolvedValue;
+              if(i<success.length){
+                clearInterval(interval);
+                interval = null;
+                i++;
+                iterate(success[i]);
+              }
+              
+             }
+          },0);
+        }else if(interval ==null){
+          i++;
+          if(i<success.length){
+            iterate(success[i]);
+          }
+        }
+        return;
+      }
+      if(success.length > 0){
+        iterate(success[i]);
+      }
+
     };
     this.reject = function(obj){
       var val = obj;
@@ -34,16 +59,14 @@ var $q = (function (){
 
 //Sample Example showing its usage
 var p1 = $q.defer();
-
-p1.promise.then(function success(obj){
-  return obj.name;
-}).then(function success(name){
-  return {name:name,age:25};
+var p2 = $q.defer();
+p1.then(function success(obj){
+  return p2;
 }).then(function success(obj){
   console.log(obj);
-  return obj;
 });
 
 setTimeout(function(){
-  p1.resolve({name:"sandeep"});
+  p1.resolve({name:"p1"});
+  p2.resolve({name:"p2"});
 },2000);
